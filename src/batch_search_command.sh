24f6f822164dfe272efe9d3135b7de34f4f6e3a8
Abhinav Rau <arau@google.com>
2023-07-07 21:52:22 +0000

    added batch command

---
+++ src/batch_search_command.sh
@@ -0,0 +1,22 @@
+page_size=${args[page_size]}
+input_file=${args[input_file]}
+output_file=${args[output_file]}
+output="["
+echo "doing batch from ${input_file}"
+while IFS= read -r line
+ do
+    if [ -z "$line" ]; then
+        continue;
+    else
+        output+=$(curl -s -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" "https://discoveryengine.googleapis.com/v1alpha/projects/${GCP_PROJECT_NUMBER}/locations/global/collections/default_collection/dataStores/${DATASTORE_NAME}/servingConfigs/default_search:search" -d '{ "query": "'"${line}"'", "page_size": "5", "offset": 0 , "contentSearchSpec": { "snippetSpec":{"maxSnippetCount": 3}, "summarySpec":{"summaryResultCount": 5}} }' \
+        | jq  '{summary: .summary.summaryText, link: .results[0].document.derivedStructData.link}') 
+        output+=","
+    fi
+done < "$input_file"
+output=${output::-1}
+output+="]"
+echo $output | jtbl -c > ${output_file}
+
+
+
+
