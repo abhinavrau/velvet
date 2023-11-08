b62490998ecdca2c942f727ec01481b5123e7460
abhinavrau <abhinav.rau@gmail.com>
2023-11-07 22:02:59 -0500

    detect auth error in batch mode

--- src/batch_search_command.sh
+++ src/batch_search_command.sh
@@ -11,8 +11,14 @@ while IFS= read -r line
     if [ -z "$line" ]; then
         continue;
     else
-        output+=$(curl -s -X POST -H "Authorization: Bearer $token" -H "Content-Type: application/json" "https://discoveryengine.googleapis.com/v1alpha/projects/${GCP_PROJECT_NUMBER}/locations/global/collections/default_collection/dataStores/${DATASTORE_NAME}/servingConfigs/default_search:search" -d '{ "query": "'"${line}"'", "page_size": "5", "offset": 0 , "contentSearchSpec": { "snippetSpec":{"maxSnippetCount": 3}, "summarySpec":{"summaryResultCount": 5}} }' \
-        | jq  '{summary: .summary.summaryText, link: .results[0].document.derivedStructData.link}') 
+        command_output=$(curl -s -X POST -H "Authorization: Bearer $token" -H "Content-Type: application/json" "https://discoveryengine.googleapis.com/v1alpha/projects/${GCP_PROJECT_NUMBER}/locations/global/collections/default_collection/dataStores/${DATASTORE_NAME}/servingConfigs/default_search:search" -d '{ "query": "'"${line}"'", "page_size": "5", "offset": 0 , "contentSearchSpec": { "snippetSpec":{"maxSnippetCount": 3}, "summarySpec":{"summaryResultCount": 5}} }') 
+        error=$(echo "$command_output" | jq '.error.code' )
+        if [ "$error" = '401' ]; then
+            echo "Authentication failed with credentials in GCP_ACCESS_TOKEN"
+            exit 1
+        else
+           output+=$(echo "$command_output" | jq  '{summary: .summary.summaryText, link: .results[0].document.derivedStructData.link}')
+        fi
         output+=","
     fi
 done < "$input_file"
